6ca32351015d290a15bbb0e54306a6a6f5b9f6ca
Caleb67184 <113487512+Caleb67184@users.noreply.github.com>
2023-10-01 17:57:10 -0600

    Update streamlit_app.py
    menu del desayuno

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Dinner")
+
+streamli.header("Breakfast Menu")
+streamli.text("Omega 3 & Blueberry Oatmeal")
+streamli.text("Kale, Spinach & Rocket Smoothie")
+streamli.text("Hard-Boiled Free-Range Egg")
